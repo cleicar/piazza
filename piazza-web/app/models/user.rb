b1dace7ad8c40d64cf4ae85491c45c4d5192e3ac
Cleiviane Costa <costa.cleiviane@gmail.com>
2024-06-17 23:53:23 -0300

    Add models and tests

---
+++ piazza-web/app/models/user.rb
@@ -0,0 +1,18 @@
+class User < ApplicationRecord
+  validates :name, presence: true
+
+  validates :email, 
+            format: { with: URI::MailTo::EMAIL_REGEXP }, 
+            uniqueness: { case_sensitive: false }
+
+  has_many :memberships, dependent: :destroy
+  has_many :organizations, through: :memberships
+
+  before_validation :strip_extraneous_spaces
+
+  private
+    def strip_extraneous_spaces
+      self.name = self.name&.strip
+      self.email = self.email&.strip
+    end
+end
